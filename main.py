bf0ce106af83c3405a48c8e8c2876337804d6c40
TeetyK <teetysupergame7484@gmail.com>
2026-02-14 18:14:20 +0700

    add: search , add data method

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ dotenv.load_dotenv()
 mcp = FastMCP("mcp-product")
 
 api_base = os.environ["API_base"]
+@mcp.tool()
+async def search_data(title: str) -> Any:
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"{api_base}?title={title}")
+        return response.json()
+    
+@mcp.tool()
+async def add_data(title: str, body: str) -> Any:
+    async with httpx.AsyncClient() as client:
+        response = await client.post(api_base, json={'title': title, 'body': body})
+        return response.json()['id']
 
 @mcp.tool()
 async def get_data() -> Any:
