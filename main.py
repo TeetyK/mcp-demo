868e441ca5a283951cbf95b46d40df8c3b1a32f9
TeetyK <teetysupergame7484@gmail.com>
2026-02-15 12:48:59 +0700

    adjust: RAG data hint function for AI Claude MCP know what i run function

--- main.py
+++ main.py
@@ -9,18 +9,21 @@ mcp = FastMCP("mcp-product")
 api_base = os.environ["API_base"]
 @mcp.tool()
 async def search_data(title: str) -> Any:
+    """Function Search data for mcp-product etc. blog มีรายการอะไรบ้าง"""
     async with httpx.AsyncClient() as client:
         response = await client.get(f"{api_base}?title={title}")
         return response.json()
     
 @mcp.tool()
 async def add_data(title: str, body: str) -> Any:
+    """Function add data for mcp-product etc. เพิ่มข้อมูล {title} ที่มี {body} เข้าไปหน่อย """
     async with httpx.AsyncClient() as client:
         response = await client.post(api_base, json={'title': title, 'body': body})
         return response.json()['id']
 
 @mcp.tool()
 async def get_data() -> Any:
+    """Function get data for mcp-product etc. แสดงข้อมูฃทั้งหมดให้หน่อย all data"""
     async with httpx.AsyncClient() as client:
         response = await client.get(api_base)
         return response.json()
